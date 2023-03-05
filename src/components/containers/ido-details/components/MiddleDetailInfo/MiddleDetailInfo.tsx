13f39e69972376924d4bee8cd796533827bfab6f
Tin Nguyen <hopthucuatin@gmail.com>
2023-03-05 10:18:43 +0700

    feat: add code base for ido detail

---
+++ src/components/containers/ido-details/components/MiddleDetailInfo/MiddleDetailInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './MiddleDetailInfo.style';
+
+interface Props {}
+
+const MiddleDetailInfo: React.FC<Props> = () => {
+  return <S.Container>MiddleDetailInfo</S.Container>;
+};
+
+export default MiddleDetailInfo;
