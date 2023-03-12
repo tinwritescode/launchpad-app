1a06476c0100e10aa877d5475f11d72a0fa37ad9
Tin Nguyen <hopthucuatin@gmail.com>
2023-03-12 10:19:50 +0700

    feat: typechain (#10)

--- src/components/containers/farming/Farming.tsx
+++ src/components/containers/farming/Farming.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FarmingFilterBar from "./components/FarmingFilterBar";
 import FarmingTable from "./components/FarmingTable";
 import styled from "styled-components";
-import { Divider } from "antd";
+import { Divider, message } from "antd";
+import PageLayout from "../../templates/PageLayout";
+import { Staking__factory } from "../../../libs/typechain-types";
+import { env } from "../../../env.mjs";
+import { ethers } from "ethers";
 
 const FarmingContainer = styled.div`
   max-width: 1200px;
@@ -11,13 +15,32 @@ const FarmingContainer = styled.div`
 `;
 
 const Farming = () => {
+  useEffect(() => {
+    // polygon mumbai
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://rpc.ankr.com/polygon_mumbai"
+    );
+    const stakingContract = Staking__factory.connect(
+      env.NEXT_PUBLIC_STAKING_CONTRACT_ADDRESS,
+      provider
+    );
+
+    console.log(stakingContract);
+
+    stakingContract.getRewardRatio().then((result) => {
+      message.info(`Reward ratio: ${result}`);
+    });
+  }, []);
+
   return (
     <>
-      <FarmingContainer>
-        <FarmingFilterBar />
-        <Divider />
-        <FarmingTable />
-      </FarmingContainer>
+      <PageLayout>
+        <FarmingContainer>
+          <FarmingFilterBar />
+          <Divider />
+          <FarmingTable />
+        </FarmingContainer>
+      </PageLayout>
     </>
   );
 };
