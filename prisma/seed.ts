2f8ffd6d5054becad9dadaa513ded6933c9f4dfe
tuthieu <68387378+tuthieu@users.noreply.github.com>
2023-03-19 01:48:33 +0700

    Add unique field

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,29 +14,33 @@ async function main() {
             },
           })
           .then((res) => {
-            prisma.project.create({
-              data: {
-                name: "Mi Fen",
-                startTime: new Date(Date.now()),
-                endTime: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-                targetRaise: 1000000,
-                allocation: 1000000,
-                Token: {
-                  create: {
-                    name: "Mi Fen",
-                    symbol: "MIFEN",
-                    address: "0x0000",
-                    decimals: 18,
-                    totalSupply: 1000000,
+            prisma.project
+              .create({
+                data: {
+                  name: "Mi Fen",
+                  startTime: new Date(Date.now()),
+                  endTime: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+                  targetRaise: 1000000,
+                  allocation: 1000000,
+                  Token: {
+                    create: {
+                      name: "Mi Fen",
+                      symbol: "MIFEN",
+                      address: "0x0000",
+                      decimals: 18,
+                      totalSupply: 1000000,
+                    },
+                  },
+                  Chain: {
+                    connect: {
+                      id: res.id,
+                    },
                   },
                 },
-                Chain: {
-                  connect: {
-                    id: res.id,
-                  },
-                },
-              },
-            });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           })
           .catch((err) => {
             console.log(err);
@@ -52,29 +56,33 @@ async function main() {
             },
           })
           .then((res) => {
-            prisma.project.create({
-              data: {
-                name: "Mike Fence",
-                startTime: new Date(Date.now()),
-                endTime: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-                targetRaise: 1000000,
-                allocation: 1000000,
-                Token: {
-                  create: {
-                    name: "Mike Fence",
-                    symbol: "MIKEFENCE",
-                    address: "0x0000",
-                    decimals: 18,
-                    totalSupply: 1000000,
+            prisma.project
+              .create({
+                data: {
+                  name: "Mike Fence",
+                  startTime: new Date(Date.now()),
+                  endTime: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+                  targetRaise: 1000000,
+                  allocation: 1000000,
+                  Token: {
+                    create: {
+                      name: "Mike Fence",
+                      symbol: "MIKEFENCE",
+                      address: "0x00000",
+                      decimals: 18,
+                      totalSupply: 1000000,
+                    },
+                  },
+                  Chain: {
+                    connect: {
+                      id: res.id,
+                    },
                   },
                 },
-                Chain: {
-                  connect: {
-                    id: res.id,
-                  },
-                },
-              },
-            });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           })
           .catch((err) => {
             console.log(err);
