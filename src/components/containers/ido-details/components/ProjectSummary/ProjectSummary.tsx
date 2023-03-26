eba5c771e8d27ccc401d0f8df0b17d2b374b9e1a
tuthieu <68387378+tuthieu@users.noreply.github.com>
2023-03-26 10:09:26 +0700

    IDO details draft UI (#32)

--- src/components/containers/ido-details/components/ProjectSummary/ProjectSummary.tsx
+++ src/components/containers/ido-details/components/ProjectSummary/ProjectSummary.tsx
@@ -1,72 +1,126 @@
 import React from "react";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 
+import { api } from "~/utils/api";
+import { Project, ScheduleRound, TokenomicsItem } from "@prisma/client";
+
+import { Anchor, Table, Col, Row, Card } from "antd";
 import * as S from "./ProjectSummary.style";
+const Pie = dynamic(() => import("@ant-design/charts").then((mod) => mod.Pie), {
+  ssr: false,
+});
 
 interface Props {}
 
+type Column = {
+  title: string;
+  dataIndex: string;
+  key: string;
+  align?: "left" | "right" | "center";
+  render?: (text: string) => string;
+};
+
+const scheduleColumn: Column[] = [
+  {
+    title: "Round",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Opens",
+    dataIndex: "startTime",
+    key: "startTime",
+    render: (text: string) => new Date(text).toLocaleString(),
+  },
+  {
+    title: "Closes",
+    dataIndex: "endTime",
+    key: "endTime",
+    render: (text: string) => new Date(text).toLocaleString(),
+  },
+];
+
+const sortScheduleRounds = (rounds: ScheduleRound[]) =>
+  rounds.sort(
+    (a, b) => new Date(a.startTime).getTime() - new Date(b.startTime).getTime()
+  );
+
 const ProjectSummary: React.FC<Props> = () => {
+  const { id } = useRouter().query as { id: string };
+  const { data, isLoading } = api.project.getOne.useQuery({ id });
+
+  const project = data as Project;
+  const scheduleRounds = data?.ScheduleRound as ScheduleRound[];
+  const tokenomicsItems = data?.TokenomicsItem as TokenomicsItem[];
+
   return (
     <S.Container>
-      <div>
-        <div className="left">
-          <a href="#prjSummary">Project Summary</a>
-          <a href="#prjSchedule">Schedule</a>
-          <a href="#prjComparision">Comparision</a>
-          <a href="#prjTokenomics">Tokenomics</a>
-          <a href="#prjRoadmap">Roadmap</a>
-          <a href="#prjTeam">Team Mmeber</a>
-          <a href="#prjInvestors">Investors</a>
-        </div>
+      <Row>
+        <Col span={8}>
+          <Card>
+            <Anchor>
+              <Anchor.Link href="#prjSummary" title="Project Summary" />
+              <Anchor.Link href="#prjSchedule" title="Schedule" />
+              <Anchor.Link href="#prjComparision" title="Comparision" />
+              <Anchor.Link href="#prjTokenomics" title="Tokenomics" />
+              <Anchor.Link href="#prjRoadmap" title="Roadmap" />
+            </Anchor>
+          </Card>
+        </Col>
 
-        <div className="right">
+        <Col span={16}>
           <div id="prjSummary">
             <h1>Project Summary</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              tincidunt, nisl eget ultricies tincidunt, nisl nisl aliquam nisl,
-              ut aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-              ultricies tincidunt, nisl nisl aliquam nisl, ut aliquam nisl nisl
-              sit amet nisl. Sed tincidunt, nisl eget ultricies tincidunt, nisl
-              nisl aliquam nisl, ut aliquam nisl nisl sit amet nisl. Sed
-              tincidunt, nisl eget ultricies tincidunt, nisl nisl aliquam nisl,
-              ut aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-              ultricies tincidunt, nisl nisl aliquam nisl, ut aliquam nisl nisl
-              sit amet nisl. Sed tincidunt, nisl eget ultricies tincidunt, nisl
-              nisl aliquam nisl, ut aliquam nisl nisl sit amet nisl. Sed
-              tincidunt, nisl eget ultricies tincidunt, nisl nisl aliquam nisl,
-              ut aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-              ultricies tincidunt, nisl nisl aliquam nisl, ut aliquam nisl nisl
-              sit amet nisl. Sed tincidunt, nisl eget ultricies tincidunt, nisl
-              nisl aliquam nisl, ut aliquam nisl nisl sit amet nisl. Sed
-              tincidunt, nisl eget ultricies tincidunt, nisl nisl aliquam nisl,
-              ut aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-              ultricies tincidunt, nisl nisl aliquam nisl, ut aliquam nisl nisl
-              sit amet nisl. Sed tincidunt, nisl eget ultricies tincidunt, nisl
-              nisl aliquam nisl, ut aliquam nisl nisl sit amet nisl. Sed
-              tincidunt, nisl eget ultricies tincidunt, nisl nisl aliquam nisl,
-              ut aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-              ultricies tincidunt, nisl nisl aliquam nisl, ut aliquam nisl nisl
-              sit amet nisl. Sed tincidunt, nisl eget ultricies tincidunt, nisl
-              nisl aliquam nisl, ut aliquam nisl nisl sit amet nisl. Sed
-              tincidunt, nisl eget ultricies tincidunt, nisl nisl aliquam nisl,
-              ut aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-              ultricies tincidunt, nisl nisl aliquam nisl, ut aliquam nisl nisl
-              sit amet nisl. Sed tincidunt, nisl eget ultricies tincidunt, nisl
-              nisl aliquam nisl, ut aliquam nisl nisl sit amet nisl. Sed
-              tincidunt, nisl eget ultricies tincidunt, nisl nisl aliquam nisl,
-              ut aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-              ultricies tincidunt, nisl nisl aliquam nisl, ut aliquam nisl nisl
-              sit amet nisl. Sed
-            </p>
+            <p>{project?.summaryContent}</p>
+            <video controls width={500}>
+              <source src={project?.videoURL} type="video/mp4" />
+            </video>
           </div>
-          <div id="prjSchedule">Schedule</div>
-          <div id="prjComparision">Comparision</div>
-          <div id="prjTokenomics">Tokenomics</div>
-          <div id="prjRoadmap">Roadmap</div>
-          <div id="prjTeam">Team Mmeber</div>
-          <div id="prjInvestors">Investors</div>
-        </div>
-      </div>
+
+          <div id="prjSchedule">
+            <h1>Schedule</h1>
+            <Table
+              dataSource={sortScheduleRounds(scheduleRounds ?? [])}
+              columns={scheduleColumn}
+              pagination={false}
+            />
+          </div>
+
+          <div id="prjComparision">
+            <h1>Comparision</h1>
+            <p>{project?.comparisionContent}</p>
+          </div>
+
+          <div id="prjTokenomics">
+            <h1>Tokenomics</h1>
+            <Pie
+              data={tokenomicsItems}
+              angleField="value"
+              colorField="name"
+              appendPadding={10}
+              radius={0.8}
+              label={{
+                type: "outer",
+                content: "{name} {value}%",
+              }}
+              interactions={[
+                {
+                  type: "pie-legend-active",
+                },
+                {
+                  type: "element-active",
+                },
+              ]}
+            />
+          </div>
+
+          <div id="prjRoadmap">
+            <h1>Roadmap</h1>
+            <p>{project?.roadmapContent}</p>
+          </div>
+        </Col>
+      </Row>
     </S.Container>
   );
 };
--- src/pages/ido/[id].tsx
+++ src/pages/ido/[id].tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import React from "react";
 import {
   MiddleDetailInfo,
@@ -7,27 +6,11 @@ import {
 } from "../../components/containers/ido-details/components";
 import PageLayout from "../../components/templates/PageLayout";
 
-import { api } from "~/utils/api";
-
 type Props = {};
 
 function IDODetail({}: Props) {
-  const router = useRouter();
-  const { id } = router.query as { id: string };
-
-  if (id === undefined) {
-    return <div>Loading...</div>;
-  }
-
-  const { data, isLoading } = api.project.getOne.useQuery({ id });
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <PageLayout>
-      {JSON.stringify(data)}
       <TopDetailInfo />
       <MiddleDetailInfo />
       <ProjectSummary />
