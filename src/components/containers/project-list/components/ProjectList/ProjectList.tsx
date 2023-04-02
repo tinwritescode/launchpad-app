2031465c6442a69e4ecdeedb40701fef08853377
Mie6996 <nguyenxuanphat060301@gmail.com>
2023-04-02 17:37:10 +0700

    Desgin modal create ido

--- src/components/containers/project-list/components/ProjectList/ProjectList.tsx
+++ src/components/containers/project-list/components/ProjectList/ProjectList.tsx
@@ -1,21 +1,24 @@
 import {
+  Box,
   Button,
   DialogContent,
   DialogTitle,
   Drawer,
+  Modal,
+  Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  Paper,
+  Typography,
 } from "@mui/material";
 import { create } from "zustand";
 import { api } from "~/utils/api";
 import { Create } from "../../../create-ido";
-import * as S from "./ProjectList.style";
 import { ProjectInfo } from "../IDOList/IDOList.style";
+import * as S from "./ProjectList.style";
 interface Props {}
 
 const projectListStore = create<{ open: boolean; toggleModal: () => void }>(
@@ -51,18 +54,12 @@ const ProjectList: React.FC<Props> = () => {
   return (
     <S.Container>
       <S.UserInfoCard
-        name='User Name'
-        role='User Role'
-        description='User Description'
-        img='https://picsum.photos/200/300'
-        link='http://localhost:3000/'
+        name="User Name"
+        role="User Role"
+        description="User Description"
+        img="https://picsum.photos/200/300"
+        link="http://localhost:3000/"
       />
-      <Drawer open={open} onClose={toggleModal} anchor='right'>
-        <DialogTitle>Create IDO</DialogTitle>
-        <DialogContent>
-          <Create />
-        </DialogContent>
-      </Drawer>
       <S.TopAction>
         <Button onClick={toggleModal}>Create IDO</Button>
       </S.TopAction>
@@ -70,28 +67,77 @@ const ProjectList: React.FC<Props> = () => {
         <Table sx={{ minWidth: 600 }}>
           <TableHead>
             <TableRow sx={{ backgroundColor: "#fbe9e7" }}>
-              <TableCell align='left'>Project Name</TableCell>
-              <TableCell align='left'>Token</TableCell>
-              <TableCell align='center'>Create At</TableCell>
-              <TableCell align='center'>Status</TableCell>
+              <TableCell align="left">Project Name</TableCell>
+              <TableCell align="left">Token</TableCell>
+              <TableCell align="center">Create At</TableCell>
+              <TableCell align="center">Status</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {data?.map((row) => (
               <TableRow key={row.name}>
-                <TableCell align='left'>
+                <TableCell align="left">
                   <ProjectInfo id={row.id} name={row.name} image={row.image} />
                 </TableCell>
-                <TableCell align='left'>{row.token?.name}</TableCell>
-                <TableCell align='center'>
+                <TableCell align="left">{row.token?.name}</TableCell>
+                <TableCell align="center">
                   {formatDate(row.createdAt)}
                 </TableCell>
-                <TableCell align='center'>{row.status}</TableCell>
+                <TableCell align="center">{row.status}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+
+      <S.TopAction>
+        <Button onClick={toggleModal}>Create IDO</Button>
+      </S.TopAction>
+      <Modal
+        open={open}
+        onClose={toggleModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{
+          p: 4,
+          width: "80%",
+          marginX: "auto",
+          overflowY: "scroll",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            pt: 4,
+            pb: 4,
+            outline: 0,
+            borderRadius: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "background.paper",
+            boxShadow: 24,
+          }}
+        >
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{
+              fontWeight: "bold",
+              size: "1.5rem",
+            }}
+          >
+            Create IDO
+          </Typography>
+          <Typography id="modal-modal-description">
+            <Create />
+          </Typography>
+        </Box>
+      </Modal>
     </S.Container>
   );
 };
