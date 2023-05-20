618ae30ca984daedb99ac617d15493dd8e874c13
tinwritescode <hopthucuatin@gmail.com>
2023-05-20 20:03:59 +0700

    build fixes

--- packages/contracts/scripts/others/add-ts-nocheck.ts
+++ packages/contracts/scripts/others/add-ts-nocheck.ts
@@ -1,22 +1,23 @@
-import { exec } from "child_process";
-import { promisify } from "util";
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
 const execAsync = promisify(exec);
 
 async function main() {
+  const path = `./src/lib/typechain-types`;
   // Find all files in ./typechain-types/ that contain the specified import or export statement
-  const { stderr, stdout } = await execAsync(
-    "grep -rl -e 'import type \\* as interface from \"./interface\";' -e 'export \\* as interface' ./typechain-types"
+  const { stdout } = await execAsync(
+    `grep -rl -e 'import type \\* as interface from "./interface";' -e 'export \\* as interface' ${path}`
   );
 
-  const files = stdout.split("\n").filter(Boolean);
+  const files = stdout.split('\n').filter(Boolean);
 
   // Loop through each file found
   for (const file of files) {
     // Check if the file already has a @ts-nocheck comment at the top
     const hasTsNoCheck =
       exec(`head -n 1 "${file}" | grep -q '@ts-nocheck'`).toString().trim() ===
-      "";
+      '';
 
     if (!hasTsNoCheck) {
       // If not, add the comment to the top of the file
@@ -27,7 +28,7 @@ async function main() {
     }
   }
 
-  console.log("Done!");
+  console.log('Done!');
 }
 
 main();
