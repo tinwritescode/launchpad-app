e5c4cf801a5fb4b8c990156019bc4c75db560335
tuilakie <ntneik15@gmail.com>
2023-05-27 13:05:54 +0700

    update staking ui

--- packages/app/src/pages/staking/index.tsx
+++ packages/app/src/pages/staking/index.tsx
@@ -1,15 +1,17 @@
-import Head from 'next/head';
-import Staking from '../../components/containers/staking/Staking';
-import PageLayout from '../../components/templates/PageLayout';
+// import Head from 'next/head';
+// import Staking from '../../components/containers/staking/Staking';
+// import PageLayout from '../../components/templates/PageLayout';
+import Stacking from '../../components/containers/staking/stacking';
 
 const WrappedStaking = () => {
   return (
-    <PageLayout>
-      <Head>
-        <title>Staking</title>
-      </Head>
-      <Staking />
-    </PageLayout>
+    // <PageLayout>
+    //   <Head>
+    //     <title>Staking</title>
+    //   </Head>
+    //   <Staking />
+    // </PageLayout>
+    <Stacking />
   );
 };
 
