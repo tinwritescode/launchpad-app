708ba7b0c3ec4555d612ed7539eb1bf61a5ae862
The Toàn <68367691+tranthetoan7501@users.noreply.github.com>
2023-05-27 14:04:09 +0700

    Migrate for project list (#72)

--- packages/app/src/pages/ido-list/index.tsx
+++ packages/app/src/pages/ido-list/index.tsx
@@ -2,24 +2,14 @@ import { type NextPage } from 'next';
 import Head from 'next/head';
 import { IDOList } from '../../components/containers/project-list/components';
 import PageLayout from '../../components/templates/PageLayout';
+import { Counter, Project } from '@strawberry/ui';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Home</title>
-      </Head>
-
-      <PageLayout
-        style={{
-          display: 'grid',
-          gap: '1rem',
-        }}
-      >
-        {/* <PageTopHeading /> */}
-        <IDOList />
-        {/* <ApplyForIDOHeading /> */}
-      </PageLayout>
+      <Project.Upcoming data={null} />
+      <Counter />
+      <Project.UpcommingTwo data={null} />
     </>
   );
 };
